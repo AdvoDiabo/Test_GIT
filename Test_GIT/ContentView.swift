522abecb86ef51b4a988db6c7e32f54b33021358
Paul Socke <Paul.Socke@hotmail.de>
2021-05-14 18:34:44 +0200

    Initial

---
+++ Test_GIT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test_GIT
+//
+//  Created by Paul Socke on 14.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test_GIT/Test_GITApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_GITApp.swift
+//  Test_GIT
+//
+//  Created by Paul Socke on 14.05.21.
+//
+
+import SwiftUI
+
+@main
+struct Test_GITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
